32c17d1a9637382891b36847244c0ca18fe8a44e
Matt Dittes <mattdittes@gmail.com>
2022-07-19 10:16:24 -0400

    commit tree 07d41042f020dc6b97a6217e0d905ef7a3d90852

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpBlock){
+    if (pickUpBlock <= 42) {
+        return 42 - pickUpBlock;
+    } else {
+        return pickUpBlock - 42;
+    }
+}
+
+function distanceFromHqInFeet(pickUpBlock){
+    return (distanceFromHqInBlocks(pickUpBlock) * 264);
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    const traveled = distanceTravelledInFeet(start, destination);
+    if(traveled <= 400) {
+        return 0;
+    } else if(traveled <= 2000) {
+        return (traveled - 400) * .02;
+    } else if(traveled <= 2500){
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
